dce6d851277a8b32df46b6695433586dcc10c235
J-Subhradeep <subhradeeppal30@gmail.com>
2024-09-12 22:43:58 +0530

    java

---
+++ NewJavaProject/src/main/Main.java
@@ -0,0 +1,148 @@
+package main;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+//		System.out.println("hello world");
+//		System.out.printf("%2d\n", 10);
+//		System.out.print("Hello world");
+		
+		
+//		c - onek code
+//		for, while, dowhile
+		
+//		python - removed unneccesary for while 
+//		for i in list
+		
+		
+//		primitive datatype, non primitive datatype
+//		int float, double
+		
+//		int a = 0;
+		
+		
+//		java version 10 - inference
+		
+//		var a = "String";
+//		a = 0;
+		
+//		java - type safe/statically typed language
+		
+		int a = 0;
+		float b = 1.9f;
+		double d = 1.33d;
+		
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(d);
+//		
+		
+//		array init
+		
+		int [] arr = new int[5];
+		int arr2 [] = new int[5];
+		int arr3 [] = {0,1,2,3};
+//		["Stri", 12];
+		
+		arr[0] = 12;
+		arr[1] = 13;
+		arr[2] = 14;
+		arr[3] = 5;
+		arr[4] = 11;
+		
+//		loops 
+		
+//		for(int i = 0; i< arr.length; i++) {
+//			System.out.println(arr[i]);
+//		}
+//		
+		
+		
+		
+//		int i = 0;
+//		while(i<arr.length) {
+//			System.out.println(arr[i]);
+//			
+//			i++;
+//		}
+		
+		
+		
+//		for (int i : arr) System.out.println(i);
+		
+		
+//		int value = 13;
+//		if(condition)
+//		{}
+//		else if{}
+//		else {}
+		
+//		switch (value) {
+//		case 10: {
+//			
+//			System.out.println("value 10");
+//			break;
+//		}
+//		case 11: {
+//			
+//			System.out.println("value 11");
+//			break;
+//		}
+//		case 12: {
+//			
+//			System.out.println("value 12");
+//			break;
+//		}
+//		default:
+//			System.out.println("Unexpected Value");
+//		}
+		
+		
+		
+//		input()
+//		scanf()
+		
+		
+//		input
+//		System.out.println("Please enter a value");
+//		
+//		Scanner sc = new Scanner(System.in);
+//		int inp = sc.nextInt();
+//		
+//		
+////		print("value",inp)
+//		System.out.println("User added : "+inp);
+		
+		
+//		float f = sc.nextFloat();
+		
+//		String name = "Subhradeep";
+//		st = st+" abc";
+//		String st2 = st+" S t2";
+//		System.out.println(name);
+		
+		
+//		String name = "Subhradeep";
+//		char c = 'p';
+//		name = name + c;
+//		
+//		System.out.println(name);
+		
+//		print, loop, array, primitive datatype, switch case, if else, string basic, user input
+		
+		
+		
+		Arrays.sort(arr);
+		
+		for(int i : arr) System.out.println(i);
+		
+		
+		int binarySearch = Arrays.binarySearch(arr, 12);
+		System.out.println("Index "+binarySearch);
+
+		
+	}
+}
